a4bdf9779e08273e7a984151ce02935e2bac4fae
sE7rgo <se7rgo@gmail.com>
2020-07-03 23:59:11 +0000

    OOP

--- index.js
+++ index.js
@@ -1,29 +1,68 @@
 let balance = 500.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
-    this.amount = amount;
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+
+  get balance() {
+    // Calculate the balance using the transaction objects.
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+
+}
+
+class Transaction {
+
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    // Keep track of the time of the transaction
+    this.time = new Date();
+    // Add the transaction to the account
+    this.account.addTransaction(this);
   }
 
 }
 
 
+class Deposit extends Transaction {
 
+  get value() {
+    return this.amount
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+  isAllowed() {
+
+    return (this.account.balance - this.amount >= 0);
+  }
+
+}
+
+
+const myAccount = new Account("serg");
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
-
-console.log('Balance:', balance);
+t2 = new Deposit(150, myAccount);
+t2.commit()
+console.log(myAccount.balance);
